6110c3c9ec1ae024b6395eb8b06fe8aaa60917dc
Wolfit0 <roblessebastian748@gmail.com>
2025-05-17 20:21:54 -0400

    Implementación de fondo de pantalla

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import Login from './Components/login';
 import Register from './Components/register';
 import './styles/App.css';
 
-
 function App() {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div className="App">
-      <h1>Bienvenido a la App</h1>
-      <button className="toggle-button" onClick={() => setIsLogin(!isLogin)}>
+    <div className="background-container">
+      <div className="gif-left" style={{ backgroundImage: "url('/cat-eat.gif')" }} />
+      <div className="gif-right" style={{ backgroundImage: "url('/cat-eat.gif')" }} />
+      <div className="overlay"></div>
+
+      <div className="App">
+        <h1>Welcome al cinema de Coquimbo York</h1>
+        <button className="toggle-button" onClick={() => setIsLogin(!isLogin)}>
           {isLogin ? '¿No tienes cuenta? Regístrate' : '¿Ya tienes cuenta? Inicia sesión'}
-      </button>
-      {isLogin ? <Login /> : <Register />}
+        </button>
+        {isLogin ? <Login /> : <Register />}
+      </div>
     </div>
   );
 }
+
 
 export default App;
